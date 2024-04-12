d0d4060769f7ce34db1e5cfbc8864d16bebc73c0
Shinsetsu <131150479+Shinsetsuwu@users.noreply.github.com>
2024-04-12 09:07:00 +0200

    menu + page accordeon

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,23 @@
 <script setup lang="ts">
-import { onErrorCaptured } from 'vue'
+import { onErrorCaptured, ref } from 'vue'
 import { RouterLink, RouterView } from 'vue-router/auto'
 
 onErrorCaptured((err, instance, info) => {
   console.error('erreur : ', err, '\ninfo : ', info, '\ncomposant : ', instance)
   return true
 })
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const menuIsopen = ref(false)
 </script>
 
 <template>
   <header>
-    <nav>
+    <!-- nav#mainNav>ul>li*3>a[href="#"]{item $} -->
+    <nav id="mainNav">
       <ul>
-        <li>
-          <RouterLink to="/" class="text-red-500 underline"> Accueil </RouterLink>
-        </li>
+        <li><RouterLink to="/" class="text-red-300 underline"> Accueil </RouterLink></li>
+        <li><RouterLink to="/accordeon" class="text-red-500 underline"> Accodeon </RouterLink></li>
       </ul>
     </nav>
   </header>
@@ -23,4 +26,11 @@ onErrorCaptured((err, instance, info) => {
       <component :is="Component" />
     </Suspense>
   </RouterView>
+  <button
+    aria-controls="mainNav"
+    aria-expanded="true"
+    class="rounded-full border-2 border-red-600 bg-red-300 px-2"
+  >
+    menu
+  </button>
 </template>
---
+++ src/pages/accordeon.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+//
+</script>
+
+<template>
+  <h1 class="text-2xl">Accordéon</h1>
+</template>
